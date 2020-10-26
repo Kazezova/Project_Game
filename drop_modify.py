993c6aec3a059384c3d6a28806954b8d01fb8a29
Kazezova <anar.kazezova@gmail.com>
2020-10-27 03:46:55 +0500

    change platform

--- drop_modify.py
+++ drop_modify.py
@@ -1,19 +1,25 @@
-import pygame
 import sys
+import pygame
+from random import randint as rand
 pygame.init()
 size = (450, 750)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pix Drop")
 icon = pygame.transform.scale(pygame.image.load("pix.png"), (32, 32))
 pygame.display.set_icon(icon)
-back_img = pygame.image.load("angryimg.png")
-cloud_img = pygame.image.load("cloud.png")
-opp = pygame.image.load("download.png")
-cloud_x = 400
-cloud_dx = -1
 clock = pygame.time.Clock()
 fps = 60
-click = False
+platform_images = ["platform_long.png", "platform_short.png"]
+enemy_images = ["kill_long.png", "kill_short.png"]
+py_platform = [(pygame.image.load(i), i) for i in platform_images]
+py_enemy = [(pygame.image.load(i), i) for i in enemy_images]
+data = [
+    [0, 100,  py_platform[rand(0, len(py_platform)-1)]], 
+    [rand(0,200), 200, py_platform[rand(0, len(py_platform)-1)]], 
+    [rand(0,200), 300, py_enemy[rand(0, len(py_enemy)-1)]], 
+    [rand(0,200), 400, py_enemy[rand(0, len(py_enemy)-1)]],
+    [rand(0,200), 500, py_enemy[rand(0, len(py_enemy)-1)]]
+]
 
 class Pix:
     dy = 7
@@ -23,7 +29,6 @@ class Pix:
         self.width = image.get_width()
         self.x = x
         self.y = y
-        self.rect = self.image.get_rect()
     def draw(self, size=None):
         if size == None:
             screen.blit(self.image, (self.x, self.y))
@@ -41,15 +46,14 @@ class Pix:
         return False
 
 class Platform:
-    def __init__(self, x, y, image):
+    def __init__(self, x, y, image, picname):
         self.image = image
+        self.picname = picname
         self.x = x
         self.y = y
         self.height = image.get_height()
         self.width = image.get_width()
         self.dx = 3
-        self.rect = self.image.get_rect()
-        
     def move(self):
         self.x += self.dx
         if self.x <= 0:
@@ -58,18 +62,18 @@ class Platform:
             self.dx *= -1
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
-    def generator(ene):
-        new = Platform(ene.x, ene.y, pygame.image.load("platform_short.png"))
-        return new  
     def __del__(self):
         pass
 
 class Enemy(Platform):
-    def __init__(self, x, y, image):
-        super().__init__(x, y, image)
-    def generator():
-        new = Enemy(20, 600, pygame.image.load("platform1_kill.png"))
-        return new  
+    def __init__(self, x, y, image, picname):
+        super().__init__(x, y, image, picname)
+
+back_img = pygame.image.load("angryimg.png")
+cloud_img = pygame.image.load("cloud.png")
+opp = pygame.image.load("download.png")
+cloud_x = 400
+cloud_dx = -1
 
 def background():
     global cloud_x
@@ -81,7 +85,8 @@ def background():
     screen.blit(opp, (cloud_x+50, 50))
     screen.blit(opp, (cloud_x, 150))
     screen.blit(opp, (cloud_x-150, 300))
-        
+
+click = False        
 def menu(): 
     pix_Img = pygame.image.load("pix.png")
     pix_Img = pygame.transform.scale(pix_Img, (pix_Img.get_width()//2, pix_Img.get_height()//2))
@@ -116,42 +121,36 @@ def menu():
         clock.tick(fps)
 
 def game(pix_Img):
+    global data
     pix_X = 70
     pix_Y = -20
     my_pix = Pix(pix_X, pix_Y, pix_Img)
-    
-    platforms = []
-    enemys = []
-    plat_Img = pygame.image.load("platform_long.png")
-    plat_X = 0
-    plat_Y = 100
-    data = [[0, 100, "platform_long.png"], [0, 200, "platform_short.png"], [0, 300, "platform1_kill.png"], 
-    [0, 400, "platform2_kill.png"],
-    [0, 500, "platform1_kill.png"]]
-    for i in range(2):
-        platforms.append(Platform(data[i][0], data[i][1], pygame.image.load(data[i][2])))
-    for i in range(2, 5):
-        enemys.append(Enemy(data[i][0], data[i][1], pygame.image.load(data[i][2])))
+
+    platforms = [Platform(data[i][0], data[i][1], data[i][2][0], data[i][2][1]) for i in range(2)]
+    enemys = [Enemy(data[i][0], data[i][1], data[i][2][0], data[i][2][1]) for i in range(2,5)]
+
     running = True
     collide = False
+
     while running:
         background()
-        my_pix.fall()
         col = my_pix.collide(platforms[0])
-        if my_pix.y > platforms[0].y + platforms[0].height and col == False:
-            print('Game OVER!')
-        for i in range(len(platforms)):
-            platforms[i].move()
-        for i in range(len(enemys)):
-            enemys[i].move()
         if col==False:
             my_pix.draw((my_pix.width, my_pix.height+20))
+            my_pix.fall()
         else:
             my_pix.draw()
+
+        if my_pix.y > platforms[0].y + platforms[0].height and col == False:
+            print('Game OVER!')
+
         for i in range(len(platforms)):
+            platforms[i].move()
             platforms[i].draw()
         for i in range(len(enemys)):
+            enemys[i].move()
             enemys[i].draw()
+            
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -160,21 +159,27 @@ def game(pix_Img):
                 if event.key == pygame.K_ESCAPE:
                     running = False
                     menu()
-                if event.key == pygame.K_SPACE:
-                    my_pix.fall()
-                    del platforms[0]
-                    platforms.append(Platform.generator(enemys[0]))
-                    del enemys[0]
-                    enemys.append(Enemy.generator())
+                # if event.key == pygame.K_SPACE:
+                #     my_pix.fall()
+                #     del platforms[0]
+                #     platforms.append(Platform.generator(enemys[0]))
+                #     del enemys[0]
+                #     enemys.append(Enemy.generator())
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    my_pix.fall()
                     del platforms[0]
-                    platforms.append(Platform.generator(enemys[0]))
+                    my_pix.fall()
+                    if 'long' in enemys[0].picname:
+                        image = py_platform[0]
+                    elif 'short' in enemys[0].picname:
+                        image = py_platform[1]
+                    platforms.append(Platform(enemys[0].x, enemys[0].y, image[0], image[1]))
                     del enemys[0]
-                    enemys.append(Enemy.generator())
+                    new = data[rand(2,4)]
+                    enemys.append(Enemy(new[0], enemys[-1].y+100, new[2][0], new[2][1]))
 
         pygame.display.flip()
         clock.tick(fps)    
+
 menu()
 pygame.quit()
